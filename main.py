4036e53391aad4d4294a4ab1a88ef862d5562cc8
Gonçalo M <dnigamerofficial@gmail.com>
2024-01-17 14:06:34 +0000

    Updated main.py. - Created functions for precreate and upload files to TB cloud.  - Fixed bugs.  - Changed structure and logging messages.
    TODO: Implement big files uploading by pieces.

--- main.py
+++ main.py
@@ -72,8 +72,7 @@ print("ii INFO: Loaded settings.")
 if os.path.exists(f"./temp"):
     print("ii INFO: Cleaning up temp directory...")
     for filename in os.listdir(f"./temp"):
-        if re.search(r"piece+\d", filename):
-            os.remove(os.path.join(sourceloc, filename))
+        os.remove(os.path.join("./temp/", filename))
     print("ii INFO: Temp directory cleared.")
 else:
     print("ii INFO: Creating temp directory...")
@@ -98,9 +97,70 @@ def convert_size(size_bytes: int):
     return f"{size} {size_name[i]}"
 
 
+def precreate_file(filenam: str, md5json: str):
+    """Precreates a file for upload"""
+    try:
+        preresponse = requests.post(f"{baseurltb}/api/precreate",
+                                    headers={"User-Agent": useragent, "Origin": baseurltb,
+                                             "Referer": baseurltb + "/main?category=all",
+                                             "Content-Type": "application/x-www-form-urlencoded"},
+                                    cookies=cookies,
+                                    data={
+                                        "app_id": "250528",
+                                        "web": "1",
+                                        "channel": "dubox",
+                                        "clienttype": "0",
+                                        "jsToken": f"{jstoken}",
+                                        "path": f"{remoteloc + "/" + filenam}",
+                                        "autoinit": "1",
+                                        "target_path": f"{remoteloc}",
+                                        "block_list": f"{md5json}",
+                                    })
+        if "uploadid" in preresponse.text:
+            return json.loads(preresponse.text)["uploadid"]
+        else:
+            print(f"!! ERROR: File precreate failed. Server returned status code {preresponse.status_code}.")
+            print(f"!! ERROR: More information: {json.loads(preresponse.text)}")
+            return "fail"
+    except Exception as e:
+        print(f"!! ERROR: File precreate failed. Server returned status code {preresponse.status_code}.")
+        print(f"!! ERROR: More information: {e}")
+        return "fail"
+
+
+def upload_file(filename: str, uploadid: str, md5hash: str):
+    """Uploads a file"""
+    try:
+        upresponse = requests.post(
+            f"{baseurltb.replace('www', 'c-jp')}/rest/2.0/pcs/superfile2?method=upload",
+            headers={"User-Agent": useragent, "Origin": baseurltb},
+            cookies=cookies,
+            files={"file": open(f"{sourceloc}/{filename}", "rb")},
+            params={
+                "type": "tmpfile",
+                "app_id": "250528",
+                "path": f"{remoteloc + '/' + filename}",
+                "uploadid": uploadid,
+                "partseq": "0",
+            },
+        )
+        print(upresponse.text)
+        if upresponse.status_code == 200:
+            md5serv = upresponse.json()["md5"]
+            if md5serv == md5hash:
+                print(f"ii UPLOAD: MD5 hash match for file {file["name"]} after upload.")
+                return md5serv
+            else:
+                print(f"ii ERROR: MD5 hash mismatch for file {file["name"]} after upload. Skipping file...")
+                return "mismatch"
+    except Exception as e:
+        print(f"!! ERROR: File upload failed. Server returned status code {upresponse.status_code}.")
+        print(f"!! ERROR: More information: {e}")
+        return "failed"
+
+
 # Show files to upload
-print()
-print("Files to upload:")
+print("\nFiles to upload:")
 for filename in os.listdir(sourceloc):
     fsizebytes = os.path.getsize(os.path.join(sourceloc, filename))
     fsize = convert_size(fsizebytes)
@@ -120,6 +180,9 @@ print(f"\nii INFO: You are a {'vip' if vip == 1 else 'non-vip'} user.")
 files = []
 for filename in os.listdir(sourceloc):
     if os.path.isfile(os.path.join(sourceloc, filename)):
+        if filename == ".DS_Store" or filename == "main.py" or filename == "settings.json" or filename == "secrets.json":
+            print(f"ii INFO: Skipping file {filename} because is a protected file.")
+            continue
         fsizebytes = os.path.getsize(os.path.join(sourceloc, filename))
         files.append({"name": filename, "sizebytes": fsizebytes})
 
@@ -127,12 +190,12 @@ for file in files:
     print(f"\n/\\ UPLOAD: Uploading {file['name']}...")
 
     # QUOTA CALCULATIONS
-    response = requests.get(
+    quotareq = requests.get(
         f"{baseurltb}/api/quota?checkfree=1",
         headers={"User-Agent": useragent},
         cookies=cookies,
     )
-    quota = json.loads(response.text)
+    quota = json.loads(quotareq.text)
     totquot = quota["total"]  # total quota available
     usequot = quota["used"]  # used quota
     aviquot = quota['total'] - quota['used']  # available quota
@@ -150,7 +213,7 @@ for file in files:
     if (vip == 1 and file["sizebytes"] <= 21474836479) or (vip == 0 and file["sizebytes"] <= 4294967296):
         pieces = []
         if file["sizebytes"] >= 2147483648:
-            print("ii INFO: File size is greater than 2GB. Uploading in chunks...")
+            print("ii SPLIT: File size is greater than 2GB. Uploading in chunks...")
             subprocess.run(
                 [
                     "split",
@@ -162,133 +225,98 @@ for file in files:
                     f"./temp/{file["name"]}.part",
                 ]
             )
-            print("ii INFO: Calculating MD5 hashes...")
+            print("ii MD5: Calculating MD5 hashes...")
             md5dict = []
             for i, infile in enumerate(sorted(glob.glob('./temp/' + file["name"] + '.part*'))):
                 # rename the files to have a 3-digit suffix
                 newname = f"./temp/{file["name"]}.part{i:03}"
                 os.rename(infile, newname)
                 md5dict.append(hashlib.md5(open(newname, 'rb').read()).hexdigest())
-                # add the piece file to the pieces array
+                # add the piece file path to the pieces array
                 pieces.append(newname)
             md5json = json.dumps(md5dict)
-            print("ii INFO: MD5 hashes calculated.")
+            print("ii MD5: MD5 hashes calculated.")
         else:
             md5dict = []
-            print("ii INFO: Calculating MD5 hashes...")
+            print("ii MD5: Calculating MD5 hashes...")
             md5dict.append(hashlib.md5(open(f"{sourceloc}/{file["name"]}", 'rb').read()).hexdigest())
-            print("ii INFO: MD5 hashes calculated.")
+            print("ii MD5: MD5 hashes calculated.")
             md5json = json.dumps(md5dict)
             pieces.append(f"{sourceloc}/{file["name"]}")
 
+        # Preinitialize the full file on the cloud
+        print("ii PRECREATE: Precreating file...")
+        uploadid = precreate_file(file["name"], md5json)
+        if uploadid == "fail":
+            continue
+        print(f"ii PRECREATE: Precreate for upload ID \"{uploadid}\" successful.")
         cloudpath = remoteloc + "/" + file["name"]
-        data = {
-            "app_id": "250528",
-            "web": "1",
-            "channel": "dubox",
-            "clienttype": "0",
-            "jsToken": f"{jstoken}",
-            "path": f"{cloudpath}",
-            "autoinit": "1",
-            "target_path": f"{remoteloc}",
-            "block_list": f"{md5json}",
-        }
 
-        response = requests.post(f"{baseurltb}/api/precreate",
-                                 headers={"User-Agent": useragent, "Origin": baseurltb,
-                                          "Referer": baseurltb + "/main?category=all",
-                                          "Content-Type": "application/x-www-form-urlencoded"},
-                                 cookies=cookies,
-                                 data=data)
+        if len(pieces) > 1:
+            print(f"ii UPLOAD: Number of pieces to be uploaded: {len(pieces)}")
 
-        if "uploadid" in response.text:
-            uploadid = json.loads(response.text)["uploadid"]
-            print(f"ii INFO: Precreate for upload ID \"{uploadid}\" successful.")
-            numpieces = len(pieces)
-
-            if numpieces > 1:
-                print(f"ii INFO: Number of pieces to be uploaded: {numpieces}")
-
-                for i, pi in enumerate(pieces):
-                    print(f"ii INFO: Uploading piece {pieces.index(pi) + 1} of {numpieces}...")
-                    data = {
-                        "type": "tmpfile",
-                        "app_id": "250528",
-                        "path": f"{cloudpath}",
-                        "uploadid": uploadid,
-                        "partseq": i,
-                    }
-                    response = requests.post(
-                        f"{baseurltb.replace('www', 'c-jp')}/rest/2.0/pcs/superfile2?method=upload",
-                        headers={"User-Agent": useragent, "Origin": baseurltb, "Content-Type": "multipart/form-data"},
-                        cookies=cookies,
-                        files={"file": open(pi, "rb")},
-                        params=data,
-                    )
-                    if response.status_code == 200:
-                        print(f"ii INFO: Piece {pieces.index(pi) + 1} of {numpieces} uploaded successfully.")
-                    else:
-                        print(f"!! ERROR: Piece {pieces.index(pi) + 1} of {numpieces} upload failed.")
-                        break
-            else:
-                print(f"ii INFO: Uploading file {file["name"]}...")
+            for i, pi in enumerate(pieces):
+                print(f"ii UPLOAD: Uploading piece {pieces.index(pi) + 1} of {len(pieces)}...")
                 data = {
                     "type": "tmpfile",
                     "app_id": "250528",
                     "path": f"{cloudpath}",
                     "uploadid": uploadid,
-                    "partseq": "0",
+                    "partseq": i,
                 }
-                try:
-                    response = requests.post(
-                        f"{baseurltb.replace('www', 'c-jp')}/rest/2.0/pcs/superfile2?method=upload",
-                        headers={"User-Agent": useragent, "Origin": baseurltb},
-                        cookies=cookies,
-                        files={"file": open(f"{sourceloc}/{file["name"]}", "rb")},
-                        params=data,
-                    )
-                    print(response.text)
-                    md5hash = response.json()["md5"]
-                    if md5hash == md5dict[0]:
-                        params = {
-                            "isdir": "0",
-                            "rtype": "1",
-                            "bdstoken": f"{bdstoken}",
-                            "app_id": "250528",
-                            "jsToken": f"{jstoken}"
-                        }
-                        data = {
-                            "path": f"{cloudpath}",
-                            "uploadid": f"{uploadid}",
-                            "target_path": f"{remoteloc}/",
-                            "size": f"{file["sizebytes"]}",
-                            "block_list": f"{md5json}",
-                        }
-                        response = requests.post(
-                            f"{baseurltb}/api/create",
-                            headers={"User-Agent": useragent, "Origin": baseurltb, "Content-Type": "application/x-www"
-                                                                                                   "-form-urlencoded"},
-                            cookies=cookies,
-                            params=params,
-                            data=data,
-                        )
-                        create = json.loads(response.text)
-                        if create["errno"] == 0:
-                            print(f"ii INFO: File {file["name"]} uploaded successfully.")
-                            if movefiles:
-                                print(f"ii INFO: Moving file {file["name"]} to {movetoloc}...")
-                                os.rename(f"{sourceloc}/{file["name"]}", f"{movetoloc}/{file["name"]}")
-                                print(f"ii INFO: File {file["name"]} moved successfully.")
-                    else:
-                        print(f"ii ERROR: MD5 hash mismatch for file {file["name"]} after upload. Skipping file...")
-                        print(f"ii ERROR: Expected: {md5dict[0]}")
-                        print(f"ii ERROR: Received: {md5hash}")
-                        continue
-
-                except Exception as e:
-                    print(f"!! ERROR: File upload failed. Server returned status code {response.status_code}.")
-                    print(f"!! ERROR: More information: {e}")
-                    continue
+                response = requests.post(
+                    f"{baseurltb.replace('www', 'c-jp')}/rest/2.0/pcs/superfile2?method=upload",
+                    headers={"User-Agent": useragent, "Origin": baseurltb, "Content-Type": "multipart/form-data"},
+                    cookies=cookies,
+                    files={"file": open(pi, "rb")},
+                    params=data,
+                )
+                if response.status_code == 200:
+                    print(f"ii UPLOAD: Piece {pieces.index(pi) + 1} of {len(pieces)} uploaded successfully.")
+                else:
+                    print(f"!! ERROR: Piece {pieces.index(pi) + 1} of {len(pieces)} upload failed.")
+                    break
         else:
-            print("!! ERROR: Precreate for file failed. Skipping file...")
-            continue
+            print(f"ii UPLOAD: Uploading file {file["name"]}...")
+
+            uploadhash = upload_file(file["name"], uploadid, md5dict[0])
+            if uploadhash == "failed":
+                continue
+            if uploadhash == "mismatch":
+                continue
+
+            params = {
+                "isdir": "0",
+                "rtype": "1",
+                "bdstoken": f"{bdstoken}",
+                "app_id": "250528",
+                "jsToken": f"{jstoken}"
+            }
+            data = {
+                "path": f"{cloudpath}",
+                "uploadid": f"{uploadid}",
+                "target_path": f"{remoteloc}/",
+                "size": f"{file["sizebytes"]}",
+                "block_list": f"{md5json}",
+            }
+            response = requests.post(
+                f"{baseurltb}/api/create",
+                headers={"User-Agent": useragent, "Origin": baseurltb, "Content-Type": "application/x-www"
+                                                                                       "-form-urlencoded"},
+                cookies=cookies,
+                params=params,
+                data=data,
+            )
+            create = json.loads(response.text)
+            if create["errno"] == 0:
+                print(f"ii UPLOAD: File {file["name"]} uploaded successfully.")
+                if movefiles:
+                    print(f"ii MOVE: Moving file {file["name"]} to {movetoloc}...")
+                    os.rename(f"{sourceloc}/{file["name"]}", f"{movetoloc}/{file["name"]}")
+                    print(f"ii MOVE: File {file["name"]} moved successfully.")
+
+    else:
+        print(f"!! ERROR: File {file["name"]} is too big for the type of account you have. Skipping file...")
+        print(f"!! ERROR: File size: {convert_size(file["sizebytes"])}")
+        print(f"!! ERROR: Maximum file size for your account: {'20GB' if vip == 1 else '4GB'}")
+        continue
